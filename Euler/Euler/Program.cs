ea4410ab36030d683da1a070c8afbd917a1cd5e7
Gibson Muniitha <g16m7574@ict.ru.ac.za>
2019-02-17 12:40:36 +0200

    Gibson question 7

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -10,6 +10,33 @@ namespace Euler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Q7());
+            Console.ReadLine();
+        }
+
+ 
+        static long Q7()  // Gibson Euler Question 7 
+        {
+            int count = 0;
+            int num = 2;
+            while (true)
+            {
+                if (isPrime(num)) count++;
+                if (count == 10001) break;
+                num++;
+            }
+            return num;
+
+
+        }
+
+        static bool isPrime(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
         }
     }
 }
