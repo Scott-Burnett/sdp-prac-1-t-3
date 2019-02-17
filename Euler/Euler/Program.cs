83b82d48077d769699e39e3ea99e6c038f0c71ac
Scott Burnett <G14B4098@ict.ru.ac.za>
2019-02-17 13:08:55 +0200

    scott first commit

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -10,6 +10,26 @@ namespace Euler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Question2:\nThe sum of all even fibonacci numbers which are less than 4000000 is: " + Q2(4000000)); // Question 2: Scott Burnett
+        }
+
+        static int Q2(int arg)// Question 2: Scott Burnett
+        {
+            int max = 0,
+                n = 0;
+              for (int i = fib(n); i < arg; i = fib(++n))
+                if (i % 2 == 0)
+                    max += i;
+              
+            return max;
+        }//Question 2
+
+        static int fib(int arg)// Fibonacci: Scott Burnett
+        {
+            if (arg < 2)
+                return 1;
+            else
+                return (fib(arg - 2) + fib(arg - 1));
         }
     }
 }
